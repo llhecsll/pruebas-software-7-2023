9db20b11d6d2ce4213c65abd9ddae6dfdeb07ef3
llhecsll <86922108+llhecsll@users.noreply.github.com>
2023-08-15 15:29:19 -0400

    backend-unit-test

---
+++ backend_unit_test/UnitTest1.cs
@@ -0,0 +1,43 @@
+using backend.connection;
+using backend.entidades;
+using backend.servicios;
+
+namespace backend_unit_test
+{
+    public class UnitTestUsuarios
+    {
+
+        public UnitTestUsuarios()
+        {
+            BDManager.GetInstance.ConnectionString = "workstation id=database-hermes.mssql.somee.com;packet size=4096;user id=hcayalo_SQLLogin_1;pwd=2itb6kw6gc;data source=database-hermes.mssql.somee.com;persist security info=False;initial catalog=database-hermes";
+        }
+
+        [Fact]
+        public void Usuarios_Get_Verificar_NotNull()
+        {
+            var result = UsuariosServicios.ObtenerTodo<Usuarios>();
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void Usuarios_GetById_VerificarItem()
+        {
+            var result = UsuariosServicios.ObtenerById<Usuarios>(1);
+            Assert.Equal(1, result.Id);
+        }
+
+        [Fact]
+        public void Usuarios_Insertar()
+        {
+            Usuarios usuarioTemp = new()
+            {
+                NombreCompleto = "Nombre Test",
+                UserName = "UserName Test",
+                Password = "Password Test"
+            };
+
+            var result = UsuariosServicios.InsertUsuarios(usuarioTemp);
+            Assert.Equal(1, result);
+        }
+    }
+}
